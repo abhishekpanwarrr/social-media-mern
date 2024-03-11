8123e0a765a5b046d4b39159ce4409c8a48df7b1
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-03-11 19:44:24 +0530

    added home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/loginpage/LoginPage";
 import HomePage from "./pages/homepage";
 import ProfilePage from "./pages/profilepage";
@@ -11,14 +11,26 @@ import { useMemo } from "react";
 const App = () => {
   const mode = useSelector((state: { mode: PaletteMode }) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+  console.log("isAuth", isAuth);
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
+          <Route
+            path="/"
+            element={isAuth ? <Navigate to="/home" /> : <LoginPage />}
+          />
+          <Route
+            path="/home"
+            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:userId"
+            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
