301a7f8d8a668c0ba04247470fe71daa9dff3b65
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-03-11 17:23:42 +0530

    added themes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/loginpage/LoginPage";
 import HomePage from "./pages/homepage";
 import ProfilePage from "./pages/profilepage";
+import { useSelector } from "react-redux";
+import { CssBaseline, PaletteMode, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { useMemo } from "react";
 
 const App = () => {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<LoginPage />} />
-    <Route path="/home" element={<HomePage />} />
-    <Route path="/profile/:userId" element={<ProfilePage />} />
-  </Routes>
-  </BrowserRouter>
+  const mode = useSelector((state: { mode: PaletteMode }) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
